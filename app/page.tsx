53f6000f7e23541d83ab1fcc67b9cba244e8e4d4
hellj504-afk <hellj504@gmail.com>
2026-06-12 20:28:16 +0500

    Add main layout components to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,23 @@
+"use client";
 
+import { Hero } from "@/components/sections/Hero";
+import { About } from "@/components/sections/About";
+import { Experience } from "@/components/sections/Experience";
+import { Projects } from "@/components/sections/Projects";
+import { Contact } from "@/components/sections/Contact";
+import { Header } from "@/components/layout/Header";
+import { Footer } from "@/components/layout/Footer";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black">
+      <Header />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+      <Footer />
+    </main>
+  );
+}
