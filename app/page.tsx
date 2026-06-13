1560e915de9c33358c8a1703c9001d5e7a3e3202
hellj504-afk <hellj504@gmail.com>
2026-06-13 12:02:16 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Hero } from "@/components/sections/Hero";
+
 export default function Home() {
   return (
-    <main style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', background: 'black', color: 'white' }}>
-      <h1>Hello, I'm Muhammad Umair</h1>
+    <main>
+      <Hero />
     </main>
   );
 }
