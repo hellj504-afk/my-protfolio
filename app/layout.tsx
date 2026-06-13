87a5f2d0dc2915524eb88ba88052376830335702
hellj504-afk <hellj504@gmail.com>
2026-06-13 14:29:51 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
-export const metadata = {
-  title: 'Muhammad Umair',
-  description: 'Portfolio',
-}
+import type { Metadata } from "next";
 
-export default function RootLayout({ children }) {
+export const metadata: Metadata = {
+  title: "Muhammad Umair",
+  description: "Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, backgroundColor: 'black' }}>{children}</body>
+      <body style={{ margin: 0, backgroundColor: "black" }}>{children}</body>
     </html>
-  )
+  );
 }
